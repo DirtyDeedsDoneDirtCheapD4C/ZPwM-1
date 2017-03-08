655e036a4bdaf87fc9ebf4e2c8eb0ed125ff25e9
Owieczka <kwegner@multimedia.edu.pl>
2017-03-08 23:23:09 +0100

    Prosta aplikacja z MessageBox'em

--- Lab01/source/main.cpp
+++ Lab01/source/main.cpp
@@ -2,6 +2,14 @@
 
 int WINAPI WinMain (HINSTANCE hInstance, HINSTANCE hPrevInstance, PSTR szCmdLine, int iCmdShow)
 {
-  MessageBox(0,"Moja pierwsza aplikacja","Aplikacja",MB_OK);
-
+  int iRetKey = MessageBox(0,"Podoba ci siê moja pierwsza aplikacja","Aplikacja",MB_YESNO);
+  if(iRetKey == IDYES) 
+  {
+    MessageBox(0,"To wspaniale","Aplikacja",MB_OK);
+  }
+  else
+  {
+    MessageBox(0,"A co ci siê nie podoba?","Aplikacja",MB_OK|MB_ICONQUESTION);
+  }
+  return 0;
 }
