7a7c77db389485b12f34b8c818226336713d9f22
Krzysztof Wegner <kwegner@multimedia.edu.pl>
2020-03-19 19:06:45 +0100

    Dodawanie ikon do zasobów aplikcji. Własna ikona aplikacji Własna ikona okna aplikacji

---
+++ Lab04/source/main.cpp
@@ -0,0 +1,61 @@
+#include <Windows.h>
+#include "res.h"
+
+HINSTANCE g_hInstance;
+
+INT_PTR CALLBACK DialogProc(HWND hwndDlg, UINT uMsg, WPARAM wParam, LPARAM lParam)
+{
+  switch(uMsg)
+  {
+  case WM_INITDIALOG:
+  {
+    HICON hIcon = LoadIcon(g_hInstance, MAKEINTRESOURCE(IDI_DRUGA));
+    SendMessage(hwndDlg, WM_SETICON, ICON_SMALL, (LPARAM)hIcon);
+    return FALSE;
+  }
+  case WM_COMMAND:
+    switch(HIWORD(wParam))
+    {
+    case BN_CLICKED:
+      switch(LOWORD(wParam))
+      {
+      case IDC_BUTTON1:
+        //MessageBox(hwndDlg,TEXT("Test"),TEXT("Klikniecie"),MB_OK);
+        //Zmieniæ pole text na przycisku na ten z EditBox
+        HWND hwndEditBox = GetDlgItem(hwndDlg, IDC_EDIT1); // I tried with and without this
+        int iTextLength = GetWindowTextLength(hwndEditBox);
+        CHAR szText[500];
+        GetWindowText(hwndEditBox, szText, iTextLength+1);
+
+        SetWindowText((HWND)lParam,szText);
+        return TRUE;
+      }
+    }
+    return FALSE;
+  case WM_LBUTTONDOWN:
+    CHAR szText[200];
+    wsprintf(szText,"Kliknales myszka x=%d,y=%d",LOWORD(lParam),HIWORD(lParam));
+    MessageBox(hwndDlg,szText,TEXT("Klikniecie"),MB_OK);
+    return TRUE;
+  case WM_CLOSE:
+    DestroyWindow(hwndDlg);
+    PostQuitMessage(0);
+    return TRUE;
+  }
+  return FALSE;
+}
+
+int WINAPI WinMain (HINSTANCE hInstance, HINSTANCE hPrevInstance, PSTR szCmdLine, int iCmdShow)
+{
+  g_hInstance = hInstance;
+  HWND hwndMainWindow = CreateDialog(hInstance,MAKEINTRESOURCE(IDD_MAINVIEW),NULL,DialogProc);
+  ShowWindow(hwndMainWindow,iCmdShow);
+
+  MSG msg = {};
+  while(GetMessage(&msg, NULL, 0, 0))
+  {
+    TranslateMessage(&msg);
+    DispatchMessage(&msg);
+  }
+  return 0;
+}
